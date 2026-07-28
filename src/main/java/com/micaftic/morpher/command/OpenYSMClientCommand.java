afdaf275b6d2a2af92f94a8ac860a8483900c69d
sdf123098 <robinson171@outlook.com>
2026-07-28 20:30:59 +0800

    Restore custom upload handling and OpenYSM dump

--- src/main/java/com/micaftic/morpher/command/OpenYSMClientCommand.java
+++ src/main/java/com/micaftic/morpher/command/OpenYSMClientCommand.java
@@ -1,17 +1,14 @@
 package com.micaftic.morpher.command;
 
 import com.micaftic.morpher.command.subcommands.client.CacheCommand;
-import com.micaftic.morpher.util.YSMMessageFormatter;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
-import com.micaftic.morpher.core.architectury.event.events.client.ClientCommandRegistrationEvent;
 
 
 public class OpenYSMClientCommand {
 
-    public static void registerClientCommands(CommandDispatcher<ClientCommandRegistrationEvent.ClientCommandSourceStack> commandDispatcher) {
-        LiteralArgumentBuilder<ClientCommandRegistrationEvent.ClientCommandSourceStack> root = LiteralArgumentBuilder.<ClientCommandRegistrationEvent.ClientCommandSourceStack>literal("openysm")
-                .requires(source -> true);
+    public static <S> void registerClientCommands(CommandDispatcher<S> commandDispatcher) {
+        LiteralArgumentBuilder<S> root = LiteralArgumentBuilder.literal("openysm");
 
         root.then(CacheCommand.register());
 
