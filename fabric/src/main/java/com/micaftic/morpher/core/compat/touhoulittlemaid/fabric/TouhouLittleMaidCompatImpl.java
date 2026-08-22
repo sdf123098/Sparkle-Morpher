39dadbbc2416fdb3a2ce5eb70ff898c77a91b711
sdf123098 <robinson171@outlook.com>
2026-08-23 06:41:21 +0800

    fix(maid): correct sitting state and preserve custom models

--- fabric/src/main/java/com/micaftic/morpher/core/compat/touhoulittlemaid/fabric/TouhouLittleMaidCompatImpl.java
+++ fabric/src/main/java/com/micaftic/morpher/core/compat/touhoulittlemaid/fabric/TouhouLittleMaidCompatImpl.java
@@ -39,7 +39,7 @@ public final class TouhouLittleMaidCompatImpl {
     }
 
     public static boolean isMaidSitting(LivingEntity livingEntity) {
-        return TouhouLittleMaidAccess.hasFishingHook(livingEntity);
+        return TouhouLittleMaidAccess.isSitting(livingEntity);
     }
 
     public static boolean isMaidOwnedBy(Entity entity, Player player) {
