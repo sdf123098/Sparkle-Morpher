943f7d4d781a2598a180b040fa061de6bd5977f6
sdf123098 <robinson171@outlook.com>
2026-09-23 16:43:42 +0800

    Add generation-safe Cloud entity appearance coordinator

--- src/neoforge/java/com/micaftic/morpher/client/event/ClientTickEvent.java
+++ src/neoforge/java/com/micaftic/morpher/client/event/ClientTickEvent.java
@@ -8,6 +8,7 @@ import com.micaftic.morpher.client.compat.ClientRenderCompatibilityRegistry;
 import com.micaftic.morpher.client.gui.resource.download.DownloadQueue;
 import com.micaftic.morpher.client.input.InputStateKey;
 import com.micaftic.morpher.client.upload.UploadManager;
+import com.micaftic.morpher.cloud.client.CloudClientRuntime;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.LocalPlayer;
 import net.neoforged.api.distmarker.Dist;
@@ -36,6 +37,7 @@ public final class ClientTickEvent {
         }
         tickCount++;
         InputStateKey.tick();
+        CloudClientRuntime.drainClientTasks();
         UploadManager.processPendingUploads();
         ClientRenderCompatibilityRegistry.tick();
         DownloadQueue.tick();
